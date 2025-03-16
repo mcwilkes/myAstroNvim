3c32843b06acd735ff9deb26a88292e8cdb6646b
Mark Wilkes <mcwilkes@gmail.com>
2025-03-16 15:40:43 -0600

    lower notify timeout

--- lua/user/plugins/nvim-notify.lua
+++ lua/user/plugins/nvim-notify.lua
@@ -2,7 +2,7 @@ return {
   "rcarriga/nvim-notify",
   lazy = false,
   opts = {
-    timeout = 500,
+    timeout = 300,
     max_height = function() return math.floor(vim.o.lines * 0.75) end,
     max_width = function() return math.floor(vim.o.columns * 0.60) end,
     on_open = function(win) vim.api.nvim_win_set_config(win, { focusable = false }) end,
